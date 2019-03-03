159f96ee4030d23a8b3d484294ddb1fd7a2c5314
brandonlroberts <brandonlroberts@outlook.com>
2019-03-03 13:26:38 -0500

    Through chapter 6.

--- SpyStore.Hol.Mvc/Startup.cs
+++ SpyStore.Hol.Mvc/Startup.cs
@@ -6,16 +6,25 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Diagnostics;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using SpyStore.Hol.Dal.EfStructures;
+using SpyStore.Hol.Dal.Initialization;
+using SpyStore.Hol.Dal.Repos;
+using SpyStore.Hol.Dal.Repos.Interfaces;
+using SpyStore.Hol.Mvc.Support;
 
 namespace SpyStore.Hol.Mvc
 {
     public class Startup
     {
-        public Startup(IConfiguration configuration)
+        private readonly IHostingEnvironment _env;
+        public Startup(IConfiguration configuration, IHostingEnvironment env)
         {
             Configuration = configuration;
+            _env = env;
         }
 
         public IConfiguration Configuration { get; }
@@ -23,15 +32,35 @@ namespace SpyStore.Hol.Mvc
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.Configure<CookiePolicyOptions>(options =>
-            {
-                // This lambda determines whether user consent for non-essential cookies is needed for a given request.
-                options.CheckConsentNeeded = context => true;
-                options.MinimumSameSitePolicy = SameSiteMode.None;
-            });
+            //services.Configure<CookiePolicyOptions>(options =>
+            //{
+            //    // This lambda determines whether user consent for non-essential cookies is needed for a given request.
+            //    options.CheckConsentNeeded = context => true;
+            //    options.MinimumSameSitePolicy = SameSiteMode.None;
+            //});
 
+            var connectionString = Configuration.GetConnectionString("SpyStore");
+            if (_env.IsDevelopment())
+            {
+                var path = Environment.GetEnvironmentVariable("APPDATA");
+                connectionString = connectionString.Replace("{path}", path);
+            }
+
+            services.AddDbContextPool<StoreContext>(options => options
+                .UseSqlServer(connectionString, o => o.EnableRetryOnFailure())
+                .ConfigureWarnings(warnings => warnings.Throw(RelationalEventId.QueryClientEvaluationWarning)));
 
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
+
+            services.AddScoped<ICategoryRepo, CategoryRepo>();
+            services.AddScoped<IProductRepo, ProductRepo>();
+            services.AddScoped<ICustomerRepo, CustomerRepo>();
+            services.AddScoped<IShoppingCartRepo, ShoppingCartRepo>();
+            services.AddScoped<IOrderRepo, OrderRepo>();
+            services.AddScoped<IOrderDetailRepo, OrderDetailRepo>();
+
+            services.Configure<CustomSettings>(Configuration.GetSection("CustomSettings"));
+
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -39,7 +68,20 @@ namespace SpyStore.Hol.Mvc
         {
             if (env.IsDevelopment())
             {
-                app.UseDeveloperExceptionPage();
+                if (env.IsDevelopment() || env.IsEnvironment("Local"))
+                {
+                    app.UseDeveloperExceptionPage();
+                    using (var serviceScope = app.ApplicationServices
+                        .GetRequiredService<IServiceScopeFactory>().CreateScope())
+                    {
+                        StoreDataInitializer.InitializeData(
+                            serviceScope.ServiceProvider.GetRequiredService<StoreContext>());
+                    }
+                }
+                else
+                {
+                    app.UseExceptionHandler("/Home/Error");
+                }
             }
             else
             {
@@ -47,7 +89,7 @@ namespace SpyStore.Hol.Mvc
             }
 
             app.UseStaticFiles();
-            app.UseCookiePolicy();
+            //app.UseCookiePolicy();
 
             app.UseMvc(routes =>
             {
---
+++ SpyStore.Hol.Mvc/Support/CustomSettings.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SpyStore.Hol.Mvc.Support
+{
+    public class CustomSettings
+    {
+        public CustomSettings() { }
+        public string MySetting1 { get; set; }
+        public int MySetting2 { get; set; }
+    }
+}
